767ec938818b2471bbf9e40ea3b4a96bf53f095c
roopa <roopatm6@gmail.com>
2024-11-06 16:49:25 +0530

    azure-linux-vm through seprate .tf for provider,resources,variables

---
+++ create_Azure-Resources-withTF/variable.tf
@@ -0,0 +1,90 @@
+variable "resourcegroup_name" {
+  default = "roopa_terraform_rg"
+}
+
+
+
+variable "location" {
+  default = "westus"
+}
+
+
+
+variable "address_space" {
+  default = ["10.1.0.0/16"]
+  type    = list(string)
+}
+
+
+
+variable "vnet_name" {
+  default = "terraform_vnet"
+}
+
+
+
+variable "subnet_space" {
+  default = ["10.1.0.0/24", "10.1.10.0/24"]
+  type    = list(string)
+}
+
+
+
+
+variable "subnet_name" {
+  default = "terraform_subnetnet"
+}
+
+
+
+variable "key" {
+  default = "ssh_key"
+}
+
+
+
+variable "nic_name" {
+  default = "terraform_nic"
+}
+
+
+variable "public_ip_name" {
+  default = "public_ip"
+}
+
+
+
+variable "ip_name" {
+  default = "ip"
+}
+
+
+variable "sku" {
+  default = "22.04-LTS"
+}
+
+
+variable "vm_name" {
+  default = "Roopa-ubantu-TF"
+}
+
+
+variable "admin" {
+  default = "azureuser"
+}
+
+
+variable "size" {
+  default = "Standard_B1s"
+}
+
+
+
+variable "user" {
+  default = "azureuser"
+}
+
+
+variable "nsg" {
+  default = "Roopa-nsg"
+}
